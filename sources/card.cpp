f6e0eb5bc05d3c4a7a1f386c2f2df5c57383b178
edenmorr <93930203+edenmorr@users.noreply.github.com>
2023-04-19 14:40:30 +0300

    Update card.cpp

--- sources/card.cpp
+++ sources/card.cpp
@@ -38,32 +38,3 @@ namespace ariel
 	}
 
 }
-// Card::Card (Rank rank=ACE, Suit suit = diamond):
-// Suit Card::getSuit() { return suit; }
-// Rank Card::getRank() { return rank; }
-
-// string getSuit(int value){
-//     if(value <= 13) return "diamond";
-//     if(value <= 26) return "spades";
-//     if(value <= 39) return "hearts";
-//     return "clubs";
-// }
-// string Card::RankName (Rank v) {
-// 	switch(v) {
-// 	case ACE: return "ACE";
-// 	case TWO: return "TWO";
-// 	case THREE: return "THREE";
-// 	case FOUR: return "FOUR";
-// 	case FIVE: return "FIVE";
-// 	case SIX:  return "SIX";
-// 	case SEVEN: return "SEVEN";
-// 	case EIGHT: return "EIGHT";
-// 	case NINE: return "NINE";
-// 	case TEN: return "TEN";
-// 	case JACK: return "JACK";
-// 	case QUEEN: return "QUEEN";
-// 	case KING: return "KING";
-// 	default: throw std::out_of_range(" value out of range");
-// 	}
-
-// }
