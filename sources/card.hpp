175eab21343e6525daf7ab429c7176afc62d774e
edenmorr <93930203+edenmorr@users.noreply.github.com>
2023-04-19 14:41:16 +0300

    Update card.hpp

--- sources/card.hpp
+++ sources/card.hpp
@@ -4,8 +4,6 @@
 using std::string;
 namespace ariel{
     
-// enum Rank {ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING};
-// enum Suit {diamond, club, heart, spade};
 
 
 class Card {
@@ -18,8 +16,6 @@ public:
 	string getSuit();
 	int getFaceValue();
 
-	// static std::string suitName(Suit s);
-	// static std::string RankName(Rank v);
 	string ToString();
 };
 }
@@ -27,62 +23,3 @@ public:
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//         public://constructors
-//         Card();//initialize a card with given difult values
-//         Card(suit,Rank);//initialize a card with given values
-//         //datas filed
-//         Rank rank;
-//         suit suit;
-//    // int values;
-//    struct deck{
-//      std::vector<Card> cards;
-//      std::string card_back;
-//      int max_deck=52;
-//    };
-//    void initialize(deck& deck);
-        
- 
-
-//     //   int rank;     // hold rank of card
-// 	//   string  deck; // hold deck of card           
-//     //   int value;
-
-//     //     void shuffle ();
-//     //     string getSuit() const;
-//     //     int getRank() const;
-//     //     int getValue() const;
-
-//     }
-// }
